8f52038c9f6f9aa2196813eca15ad7d866816ad7
Joshua Palacios <palacios.joshua@gmail.com>
2017-11-02 20:22:03 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,4 +1,10 @@
 def reverse_each_word(sentence)
-  sentence_array = sentence.split("")
-  return sentence_array.reverse
+  sentence_array = sentence.split
+  sentence_array.each |word| do
+    word_array = word.split
+    word_reverse = word_array.reverse 
+    word_reverse_array = word_reverse.join
+    sentence_final << word_reverse_array
+  end
+    sentence_final
 end
