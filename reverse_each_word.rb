031f9189af45c53fa1a263fc98920767f8078437
Joshua Palacios <palacios.joshua@gmail.com>
2017-11-02 20:25:32 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,11 +1,6 @@
 def reverse_each_word(sentence)
   sentence_array = sentence.split
   sentence_final = []
-  sentence_array.each |word| do
-    word_array = word.split
-    word_reverse = word_array.reverse
-    word_reverse_array = word_reverse.join
-    sentence_final << word_reverse_array
-    end
+  
   sentence_final
 end
