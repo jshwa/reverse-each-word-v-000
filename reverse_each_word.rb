eafada0ecb33aa3d149cc4353a332bdbbc005221
Joshua Palacios <palacios.joshua@gmail.com>
2017-11-02 23:34:51 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -5,7 +5,7 @@ def reverse_each_word(sentence)
   sentence_array.each do |word|
     word_array = word.split
     word_reverse = word_array.reverse
-    word_reverse_array = word_reverse.join
+    word_reverse_string = word_reverse.join
     end
   word_reverse_string
 end
