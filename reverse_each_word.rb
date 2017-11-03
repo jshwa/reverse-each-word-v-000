8e1921291941bb57999c5a1010ffd0ab74307409
Joshua Palacios <palacios.joshua@gmail.com>
2017-11-03 00:01:03 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,11 +3,11 @@ def reverse_each_word(sentence)
   sentence_array.each do |word|
     letters = word.split
     letters.reverse_each do |letter|
-      str += letter
+      str += "#{letter}"
       str.join
       str
     end
-    letters += str
+    letters << str
     letters.join
     end
     letters
